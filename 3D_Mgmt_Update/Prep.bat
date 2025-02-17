f7a3908af556682d68512c899ec52740c6e01a06
LlamasAnonymous <gjbrooks2000@gmail.com>
2025-02-16 23:25:33 -0600

    Updates

--- 3D_Mgmt_Update/Prep.bat
+++
@@ -1,4 +0,0 @@
-taskkill /im powershell.exe /f
-taskkill /im pwsh.exe /f
-
-start powershell.exe -file "C:\Versions\3D_Mgmt_Update\Setup.ps1"
--- 3D_Mgmt_Update/Rm3D.bat
+++ 3D_Mgmt_Update/Rm3D.bat
@@ -1,10 +1,14 @@
+cd \
+
 taskkill /im powershell.exe /f
 taskkill /im pwsh.exe /f
+
+Timeout 1
 
 rd "C:\3D_File_Mgmt" /s /q
 
 git clone https://github.com/LlamasAnonymous/3D_File_Mgmt.git C:\3D_File_Mgmt
 
-Start /min pwsh.exe -WindowStyle Hidden -file "C:\3D_File_Mgmt\3D_File_Mgmt.ps1"
+Start pwsh -WindowStyle Hidden "C:\3D_File_Mgmt\3D_File_Mgmt.ps1"
 
-Start pwsh.exe -file "C:\3D_File_Mgmt\Update\Version_R.ps1"
+Start pwsh "C:\3D_File_Mgmt\Update\Version_R.ps1"
